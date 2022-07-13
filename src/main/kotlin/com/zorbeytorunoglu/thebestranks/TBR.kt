714dc0a54b32c57548b3a642dc48d3969454b4b6
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2022-07-13 03:39:58 +0100

    Loading and saving data, utils, loading player ranks, registering hooks and events

--- src/main/kotlin/com/zorbeytorunoglu/thebestranks/TBR.kt
+++ src/main/kotlin/com/zorbeytorunoglu/thebestranks/TBR.kt
@@ -7,7 +7,9 @@ import com.zorbeytorunoglu.thebestranks.configuration.messages.MessageHandler
 import com.zorbeytorunoglu.thebestranks.configuration.ranks.Rank
 import com.zorbeytorunoglu.thebestranks.configuration.settings.SettingsContainer
 import com.zorbeytorunoglu.thebestranks.configuration.settings.SettingsHandler
+import com.zorbeytorunoglu.thebestranks.hooks.PAPI
 import com.zorbeytorunoglu.thebestranks.listeners.Chat
+import com.zorbeytorunoglu.thebestranks.listeners.Join
 import com.zorbeytorunoglu.thebestranks.utils.RankUtils
 import com.zorbeytorunoglu.thebestranks.utils.Utils
 import net.milkbowl.vault.permission.Permission
@@ -25,13 +27,14 @@ class TBR: JavaPlugin() {
 
     private lateinit var ranks: ArrayList<Rank>
 
-    private lateinit var playerRanks: HashMap<UUID, Rank>
+    private var playerRanks: HashMap<UUID, Rank> = HashMap()
 
     private lateinit var settingsHandler: SettingsHandler
-
     private lateinit var messageHandler: MessageHandler
 
     private var permissionHook: Permission? = null
+
+    private lateinit var utils: Utils
 
     override fun onEnable() {
 
@@ -48,21 +51,37 @@ class TBR: JavaPlugin() {
 
         messageHandler= MessageHandler(MessageContainer(configResource))
 
+        if (Bukkit.getServer().pluginManager.getPlugin("PlaceholderAPI")==null) {
+            logger.log(Level.SEVERE, "[TheBestRanks] Plugin can't work without PlaceholderAPI.")
+            Bukkit.getServer().pluginManager.disablePlugin(this)
+            return
+        } else {
+            PAPI(this).register()
+        }
+
         if (settingsHandler.getChatFormatEnabled()) {
 
             if (!setupPermissions()) {
-                logger.log(Level.WARNING,"[TheBestRanks] Vault or a proper permission system could not be found. Chat format system won't work")
+                logger.log(Level.WARNING,"[TheBestRanks] Vault or/and a proper permission system could not be found. Chat format system won't work")
             } else {
                 Bukkit.getServer().pluginManager.registerEvents(Chat(this),this)
             }
 
         }
 
+        Bukkit.getServer().pluginManager.registerEvents(Join(this),this)
+
+        utils=Utils(this)
+
         getCommand("rank").executor = CmdRank(this)
+
+        loadPlayerRanks(dataResource, playerRanks, ranks)
 
     }
 
     override fun onDisable() {
+
+        savePlayerRanks(getDataResource(),getPlayerRanks())
 
     }
 
@@ -98,6 +117,10 @@ class TBR: JavaPlugin() {
         return ranksResource
     }
 
+    fun getUtils(): Utils {
+        return utils
+    }
+
     private fun setupPermissions(): Boolean {
         if (Bukkit.getServer().pluginManager.getPlugin("Vault")==null) return false
         val rsp = Bukkit.getServer().servicesManager.getRegistration(
@@ -105,5 +128,50 @@ class TBR: JavaPlugin() {
         permissionHook = rsp.provider
         return permissionHook != null
     }
+
+    private fun savePlayerRanks(dataResource: Resource, playerRanks: HashMap<UUID, Rank>) {
+
+        if (playerRanks.isEmpty()) return
+
+        for (uuid in playerRanks.keys) {
+
+            dataResource.set("ranks.$uuid", playerRanks[uuid]!!.getId())
+
+        }
+
+        dataResource.save()
+
+    }
+
+    private fun loadPlayerRanks(dataResource: Resource, playerRanks: HashMap<UUID, Rank>, ranks: ArrayList<Rank>) {
+
+        println("hilan1")
+
+        if (!dataResource.getFile().exists()) return
+
+        println("hilan2")
+
+        if (!dataResource.contains("ranks")) return
+
+        println("hilan3")
+
+        for (key in dataResource.getConfigurationSection("ranks").getKeys(false)) {
+
+            println("hilan4")
+
+            for (rank in ranks) {
+                println("hilan5")
+                if (rank.getId() == dataResource.getString("ranks.$key")) {
+                    playerRanks[UUID.fromString(key)] = rank
+                    break
+                }
+            }
+
+        }
+
+    }
+
+    //TODO: Rewards message
+    //TODO: GUI (optional)
 
 }
