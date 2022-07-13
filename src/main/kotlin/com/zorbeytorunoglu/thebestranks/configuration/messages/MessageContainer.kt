ca8b5b2c10b296cb95dfa2d59d02c6f0a2befbbd
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2022-07-13 03:36:37 +0100

    Added some messages

--- src/main/kotlin/com/zorbeytorunoglu/thebestranks/configuration/messages/MessageContainer.kt
+++ src/main/kotlin/com/zorbeytorunoglu/thebestranks/configuration/messages/MessageContainer.kt
@@ -6,9 +6,29 @@ import com.zorbeytorunoglu.thebestranks.utils.StringUtils
 class MessageContainer {
 
     val noPerm: String
+    val yourRank: String
+    val onlyInGame: String
+    val rankUpMessage: String
+    val noGreaterRank: String
+    val rankSetUsage: String
+    val playerNotFound: String
+    val rankDoesntExists: String
+    val rankSet: String
+    val noRankPrefix: String
+    val noGreaterRankPrefix: String
 
     constructor(configResource: Resource) {
         this.noPerm=StringUtils.hex(configResource.getString("messages.no-perm"))
+        this.yourRank=StringUtils.hex(configResource.getString("messages.your-rank"))
+        this.onlyInGame=StringUtils.hex(configResource.getString("messages.only-in-game"))
+        this.rankUpMessage=StringUtils.hex(configResource.getString("messages.rank-up"))
+        this.noGreaterRank=StringUtils.hex(configResource.getString("messages.no-greater-rank"))
+        this.rankSetUsage=StringUtils.hex(configResource.getString("messages.rank-set-usage"))
+        this.playerNotFound=StringUtils.hex(configResource.getString("messages.player-not-found"))
+        this.rankDoesntExists=StringUtils.hex(configResource.getString("messages.rank-doesnt-exists"))
+        this.rankSet=StringUtils.hex(configResource.getString("messages.rank-set"))
+        this.noRankPrefix=StringUtils.hex(configResource.getString("messages.no-rank-prefix"))
+        this.noGreaterRankPrefix=StringUtils.hex(configResource.getString("messages.no-greater-rank-prefix"))
     }
 
 }
--- src/main/kotlin/com/zorbeytorunoglu/thebestranks/configuration/messages/MessageHandler.kt
+++ src/main/kotlin/com/zorbeytorunoglu/thebestranks/configuration/messages/MessageHandler.kt
@@ -8,4 +8,44 @@ class MessageHandler(private val container: MessageContainer) {
         return StringUtils.hex(container.noPerm)
     }
 
+    fun getYourRank(): String {
+        return container.yourRank
+    }
+
+    fun getOnlyInGame(): String {
+        return container.onlyInGame
+    }
+
+    fun getRankUp(): String {
+        return container.rankUpMessage
+    }
+
+    fun getNoGreaterRank(): String {
+        return container.noGreaterRank
+    }
+
+    fun getRankSetUsage(): String {
+        return container.rankSetUsage
+    }
+
+    fun getPlayerNotFound(): String {
+        return container.playerNotFound
+    }
+
+    fun getRankNotFound(): String {
+        return container.rankDoesntExists
+    }
+
+    fun getRankSet(): String {
+        return container.rankSet
+    }
+
+    fun getNoRankPrefix(): String {
+        return container.noRankPrefix
+    }
+
+    fun getNoGreaterRankPrefix(): String {
+        return container.noGreaterRankPrefix
+    }
+
 }
